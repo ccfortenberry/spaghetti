d9267f4e788a764de11f528e63628191c751b2dc
ccfortenberry <cmnky@hotmail.com>
2019-10-23 15:46:04 -0800

    Refactor to remove not needed class and add working sort

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 ***********************************/
 #include "spaghet.hpp"
 using spaghet::Noodle;
-using spaghet::NoodleBox;
 
 // iostream included in header
 using std::cout;
@@ -27,7 +26,7 @@ using std::runtime_error;
 int main() {
 	{
 		cout << "Begin lemon testing to ensure basic functionality:\n_____________________________________" << endl;
-		cout << "Testing namespace as a lemon test" << endl;
+		cout << "Testing namespace as the lemon-est lemon test" << endl;
 		spaghet::printmeme();
 	}
 	
@@ -88,384 +87,37 @@ int main() {
 	cout << endl;
 	
 	{
-		cout << "Testing default NoodleBox creation..." << endl;
-		
-		try {
-			NoodleBox<int> test;
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical NoodleBox Failure!!" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully made a box of noodles!!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing the insertion of a Noodle into a Box..." << endl;
-		
-		Noodle<int> testNood(39);
-		NoodleBox<int> testBox;
-		
-		try {
-			testBox.insertNoodle(testNood);
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical Noodle Insertion Failure!!" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully inserted a Noodle into a Box!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing the insertion of a Noodle rvalue into a Box..." << endl;
-		
-		NoodleBox<int> testBox;
-		
-		try {
-			testBox.insertNoodle(Noodle<int>(39));
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical Noodle Insertion Failure!!" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully inserted a Noodle into a Box!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing the insertion of a Noodle into a Box, with initializer list..." << endl;
-		
-		try {
-			NoodleBox<int> testBox = {39, 42, 69, 100};
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical Noodle Insertion Failure!!" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully inserted a Noodle into a Box!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing the lookup of a Noodle in a Box..." << endl;
-		
-		Noodle<int> testNoodle(39);
-		Noodle<int> compNoodle(39);
-		NoodleBox<int> testBox;
-		testBox.insertNoodle(testNoodle);
-		
-		try {
-			//Noodle(39).getVal() == testBox[0].getVal();
-			if (compNoodle.getVal() != testBox.getNoodleAt(0).getVal()) throw runtime_error("Values are not equal when they should be!");
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical Noodle Lookup Failure!!" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully found a Noodle in a Box!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing the lookup of a Noodle in a Box, but with rvalues..." << endl;
-		
-		NoodleBox<int> testBox;
-		testBox.insertNoodle(Noodle<int>(39));
-		
-		try {
-			if (testBox.getNoodleAt(0).getVal() != 39) throw runtime_error("Values are not equal when they should be!");
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical Noodle Lookup Failure!!" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully found a Noodle in a Box!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing the lookup of a Noodle in a Box, but with operator[]..." << endl;
-		
-		vector<int> noodles = {1, 2, 5, 18, 22, 39, 42, 15, 6};
-		NoodleBox<int> testBox = {1, 2, 5, 18, 22, 39, 42, 15, 6};
-		
-		try {
-			for (unsigned int i=0; i<noodles.size(); i++) {
-				if (noodles[i] != testBox[i]) throw runtime_error("Values are not equal when they should be!");
-			}
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical Noodle Lookup Failure!!" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully found all Noodles in a Box!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing to make sure that the distance of a noodle is correctly set..." << endl;
-		
-		NoodleBox<int> testBox = {10};
-		
-		try {
-			if (testBox.getNoodleAt(0).getOrder() != 0) throw runtime_error("Values are not equal when they should be!");
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical Noodle distance Failure!!" << endl;
-			cout << "(";
-			for (unsigned int i=0; i<testBox.size(); i++) cout << testBox.getNoodleAt(i).getOrder() << ",";
-			cout << ")" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully verified Noodles' length in a Box!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing to make sure that the distances from the longest noodle are correct..." << endl;
-		
-		vector<unsigned int> noodles = {1, 2, 5, 18, 22, 39, 42, 15, 6};
-		vector<unsigned int> noodmax = {8, 7, 6, 3, 2, 1, 0, 4, 5};
-		NoodleBox<unsigned int> testBox = {1, 2, 5, 18, 22, 39, 42, 15, 6};
-		
-		try {
-			for (unsigned int i=0; i<noodles.size(); i++) {
-				if (noodmax[i] != testBox.getNoodleAt(i).getOrder()) throw runtime_error("Values are not equal when they should be!");
-			}
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical Noodle distance Failure!!" << endl;
-			cout << "(";
-			for (unsigned int i=0; i<testBox.size(); i++) cout << testBox.getNoodleAt(i).getOrder() << ","; 
-			cout << ")" << endl;
-			cout << "(";
-			for (unsigned int i=0; i<testBox.size(); i++) cout << testBox[i] << ","; 
-			cout << ")" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully verified Noodles' length in a Box!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing to make sure that the index of the tallest noodle is correct..." << endl;
-		
-		unsigned int tallest = 6;
-		NoodleBox<int> testBox = {1, 2, 5, 18, 22, 39, 42, 15, 6};
-		
-		try {
-			if (testBox.getIdxOfTallest() != tallest) throw runtime_error("Values are not equal when they should be!");
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical tallest noodle index Failure!!" << endl;
-			cout << testBox.getIdxOfTallest() << " != " << tallest << endl;
-			return 1;
-		}
-		
-		cout << "Successfully verified the index of the tallest noodle!!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing to make sure that the index of the tallest noodle is correct, but with floats!..." << endl;
-		
-		unsigned int tallest = 3;
-		NoodleBox<float> testBox = {10.2, 6.8, 15.04, 22.9, 4.4};
-		
-		try {
-			if (testBox.getIdxOfTallest() != tallest) throw runtime_error("Values are not equal when they should be!");
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical tallest noodle index Failure!!" << endl;
-			cout << testBox.getIdxOfTallest() << " != " << tallest << endl;
-			return 1;
-		}
-		
-		cout << "Successfully verified the index of the tallest float noodle!!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing copying from another container..." << endl;
-		
-		vector<short> vecNood = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-		
-		try {
-			NoodleBox<short> testBox = vecNood;
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical noodle container Failure!!" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully created noodles from a vector!!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing copying from a double ended queue of negative noodles..." << endl;
-		
-		deque<long> vecNood = {-9, -8, -7, -6, -5, -4, -3, -2, -1, 0};
-		
-		try {
-			//NoodleBox<long> testBox(vecNood);
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical noodle container Failure!!" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully created noodles from a deque!!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing copying from a list of chars..." << endl;
-		
-		list<char> vecNood = {'i', 't', 'a', 'L', 'y'};
-		
-		try {
-			NoodleBox<char> testBox = vecNood;
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical noodle container Failure!!" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully created noodles from a list!!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing copying from a std::array of chars..." << endl;
-		
-		array<char,5> vecNood = {'i', 't', 'a', 'L', 'y'};
-		
-		try {
-			NoodleBox<char> testBox(vecNood.begin(), vecNood.end());
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Critical noodle container Failure!!" << endl;
-			return 1;
-		}
-		
-		cout << "Successfully created noodles from a list!!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
 		cout << "Testing sort..." << endl;
 		
-		array<int,5> vecNood = {40, 20, 50, 30, 10};
-		NoodleBox<int> testBox(vecNood.begin(), vecNood.end());
+		array<char,5> chararr = {'i', 't', 'a', 'L', 'y'};
+		vector<char> charvec(chararr.begin(), chararr.end());
 		
 		cout << "Start: ";
-		for (auto i : vecNood) cout << i << ", ";
+		for (auto i : chararr) cout << i;
 		cout << endl << "Start: ";
-		for (unsigned int i=0; i<testBox.size(); i++) cout << testBox[i] << ", ";
-		cout << endl << "Dist: ";
-		for (unsigned int i=0; i<testBox.size(); i++) cout << testBox.getNoodleAt(i).getOrder() << ", ";
+		for (unsigned int i=0; i<charvec.size(); i++) cout << charvec[i];
 		cout << endl;
 		
-		std::sort(vecNood.begin(), vecNood.end());
-		spaghet::sort(testBox);
+		std::sort(chararr.begin(), chararr.end());
+		spaghet::sort(charvec.begin(), charvec.end());
 		
 		try {
-			for(unsigned int i=0; i<testBox.size(); i++) {
-				if (vecNood[i] != testBox[i]) throw runtime_error("sorting Failure!!");
+			for(unsigned int i=0; i<charvec.size(); i++) {
+				if (chararr[i] != charvec[i]) throw runtime_error("sorting Failure!!");
 			}
 		}
 		catch (const exception & e) {
 			cout << e.what() << endl;
 			cout << "Desired: ";
-			for (auto i : vecNood) cout << i << ", ";
+			for (auto i : chararr) cout << i;
 			cout << endl << "Result: ";
-			for (unsigned int i=0; i<testBox.size(); i++) cout << testBox[i] << ", ";
+			for (unsigned int i=0; i<charvec.size(); i++) cout << charvec[i];
 			cout << endl;
 			
 			return 1;
 		}
 		
-		cout << "Successfully created noodles from a list!!" << endl;
-	}
-	
-	cout << endl;
-	
-	{
-		cout << "Testing sort..." << endl;
-		
-		array<char,5> vecNood = {'i', 't', 'a', 'L', 'y'};
-		NoodleBox<char> testBox(vecNood.begin(), vecNood.end());
-		
-		cout << "Start: ";
-		for (auto i : vecNood) cout << i;
-		cout << endl << "Start: ";
-		for (unsigned int i=0; i<testBox.size(); i++) cout << testBox[i];
-		cout << endl << "Dist: ";
-		for (unsigned int i=0; i<testBox.size(); i++) cout << testBox.getNoodleAt(i).getOrder();
-		cout << endl;
-		
-		std::sort(vecNood.begin(), vecNood.end());
-		spaghet::sort(testBox);
-		
-		try {
-			for(unsigned int i=0; i<testBox.size(); i++) {
-				if (vecNood[i] != testBox[i]) throw runtime_error("sorting Failure!!");
-			}
-		}
-		catch (const exception & e) {
-			cout << e.what() << endl;
-			cout << "Desired: ";
-			for (auto i : vecNood) cout << i;
-			cout << endl << "Result: ";
-			for (unsigned int i=0; i<testBox.size(); i++) cout << testBox[i];
-			cout << endl;
-			
-			return 1;
-		}
-		
-		cout << "Successfully created noodles from a list!!" << endl;
+		cout << "SORTED" << endl;
 	}
 	
 	// Test copy and move ctors
--- spaghet.hpp
+++ spaghet.hpp
@@ -25,8 +25,6 @@ namespace spaghet {
 		public:
 			Noodle():_val(0), _order(-1) {};
 			
-			// Calculate new noodle distance from the tallest
-			// This will be done in the box and updated accordingly
 			Noodle(const T & val):_val(val), _order(-1) {}
 			
 			Noodle(T && val):_val(std::move(val)), _order(-1) {}
@@ -51,156 +49,49 @@ namespace spaghet {
 			unsigned int _order;
 	};
 	
-	template <class T>
-	class NoodleBox {
-		public:
-			// Default Ctor
-			NoodleBox():_noodleVec(), _idxOfTallest(0) {};
-			
-			// Ctor from initializer_list
-			NoodleBox(const std::initializer_list<T> & list):_idxOfTallest(0) {
-				//_noodleVec.resize(std::distance(list.begin(), list.end()));
-				for (auto i : list) {
-					insertNoodle(Noodle<T>(i));
-				}
-			}
-			
-			// Ctor from iterators
-			template <typename Iter>
-			NoodleBox(Iter first, Iter last):_idxOfTallest(0) {
-				for (auto f=first; f!=last; f++) {
-					insertNoodle(Noodle<T>(*f));
-				}
-			}
-			
-			// Ctor from container by const reference
-			template <template <typename> class Container>
-			NoodleBox(const Container<T> & list):_idxOfTallest(0) {
-				for (auto i=list.begin(); i!=list.end(); i++) {
-					insertNoodle(Noodle<T>(*i));
-				}
-			}
-			
-			// Ctor from container by r-value reference
-			template <template <typename> class Container>
-			NoodleBox(Container<T> && list):_idxOfTallest(0) {
-				for (auto i=list.begin(); i!=list.end(); i++) {
-					insertNoodle(Noodle<T>(*i));
-				}
-			}
-			
-			// Assignment from container by const reference
-			template <template <typename> class Container>
-			NoodleBox<T> & operator=(const Container<T> & list) {
-				return *NoodleBox<T>(list);
-			}
-			
-			// Assignment from container by r-value reference
-			template <template <typename> class Container>
-			NoodleBox<T> & operator=(Container<T> && list) {
-				return *NoodleBox<T>(std::move(list));
-			}
-			
-			// Copy ctor
-			NoodleBox(const NoodleBox<T> & other):_idxOfTallest(other.getIdxOfTallest()) {
-				for (std::size_t i=0; i<other.size(); i++) {
-					insertNoodle(other[i]);
-				}
-			}
-			
-			// Move ctor
-			NoodleBox(NoodleBox<T> && other):_idxOfTallest(std::move(other.getIdxOfTallest())) {
-				for (std::size_t i=0; i<other.size(); i++) {
-					insertNoodle(other[i]);
-				}
-			}
-			
-			// Copy assignment
-			NoodleBox<T> & operator=(const NoodleBox<T> & other) {
-				return *NoodleBox<T>(other);
-			}
-			
-			// Move assignment
-			NoodleBox<T> & operator=(NoodleBox<T> && other) {
-				return *NoodleBox<T>(std::move(other));
-			}
-			
-			// Dtor
-			~NoodleBox()=default;
-			
-			Noodle<T> getTallest() const {
-				return _noodleVec[_idxOfTallest];
-			}
-			
-			unsigned int getIdxOfTallest() const {
-				return _idxOfTallest;
-			}
-			
-			std::size_t size() const {
-				return _noodleVec.size();
-			}
-			
-			void insertNoodle(const Noodle<T> & noodle) {
-				_noodleVec.push_back(noodle);
-				
-				for (unsigned int i=0; i<_noodleVec.size(); i++) {
-					if (_noodleVec[size()-1].getVal() > _noodleVec[i].getVal()) {
-						_noodleVec[i].setOrder(_noodleVec[i].getOrder()+1);
-					}
-					else {
-						_noodleVec[size()-1].setOrder(_noodleVec[size()-1].getOrder()+1);
-					}
-				}
-				if (_noodleVec[size()-1].getVal() > _noodleVec[_idxOfTallest].getVal()) _idxOfTallest = size()-1;
-			}
-			
-			void insertNoodle(Noodle<T> && noodle) {
-				_noodleVec.push_back(std::move(noodle));
-				
-				for (unsigned int i=0; i<_noodleVec.size(); i++) {
-					if (_noodleVec[size()-1].getVal() > _noodleVec[i].getVal()) {
-						_noodleVec[i].setOrder(_noodleVec[i].getOrder()+1);
-					}
-					else {
-						_noodleVec[size()-1].setOrder(_noodleVec[size()-1].getOrder()+1);
-					}
-				}
-				if (_noodleVec[size()-1].getVal() > _noodleVec[_idxOfTallest].getVal()) _idxOfTallest = size()-1;
-			}
-			
-			// allow insert by a value instead of a raw noodle?
-			
-			Noodle<T> getNoodleAt(const unsigned int & index) const {
-				return _noodleVec[index];
-			}
-			
-			T operator[](const unsigned int & index) const {
-				return _noodleVec[index].getVal();
-			}
-			
-			void removeNoodle(const unsigned int & noodleIdx) {
-				_noodleVec.erase(_noodleVec.begin()+noodleIdx);
-				// Adjust elements to have accurate distFromTallest
-			}
-		private:
-			//std::vector<std::unique_ptr<Noodle>> _noodleVec; taking references to noodles instead of noodle
-			//objects as an optimization. But would we want to have a box of noodles that only contain the
-			//locations of noodles instead of actual noodles themselves? sounds unappetizing to me...
-			std::vector<Noodle<T> > _noodleVec;
-			unsigned int _idxOfTallest;
-	};
-	
-	template <typename T>
-	void sort(NoodleBox<T> & b) {
+	template <typename Iter>
+	void sort(Iter first, Iter last) {
+		std::vector<Noodle<typename std::iterator_traits<Iter>::value_type>> temp;
+		auto it = first;
+		auto size = std::distance(first, last);
+		unsigned int maxIdx = 0;
+		unsigned int max = 0;
 		
-	}
-	
-	template <typename RAIter>
-	void sort(RAIter first, RAIter last) {
-		NoodleBox<typename std::iterator_traits<RAIter>::value_type> temp(first, last);
-		sort(temp);
-		for (unsigned int i=0; i<temp.size(); i++)
-			*first++ = temp[i];
+		// Naive insertion and max tracking
+		// O(n*lg(n))
+		for (unsigned int i=0; i<size; i++) {
+			temp.push_back(*it++);
+			auto width = temp.size();
+			
+			for (unsigned int j=0; j<width; j++) {
+				if (temp[temp.size()-1].getVal() > temp[j].getVal()) 
+					temp[j].setOrder(temp[j].getOrder()+1);
+				else 
+					temp[temp.size()-1].setOrder(temp[temp.size()-1].getOrder()+1);
+			}
+			
+			if (temp[i].getVal() > temp[maxIdx].getVal()) maxIdx = size-1;
+		}
+		
+		// Debug output
+		std::cout << "Dist: ";
+		for (auto i : temp) std::cout << i.getOrder();
+		std::cout << std::endl << "Val: ";
+		for (auto i : temp) std::cout << i.getVal();
+		std::cout << std::endl << "Max idx: " << maxIdx << std::endl;
+		
+		// The Very Naive Sort
+		// O(n*lg(n))...
+		do {
+			*--last = temp[maxIdx].getVal();
+			max++;
+			for (unsigned int i=0; i<size; i++) {
+				if (temp[i].getOrder() == max) {
+					maxIdx = i;
+					break;
+				}
+			}
+		} while (first != last);
 	}
 }
 
